3fdb13137665b75ac6396686771ab686a97bbe20
AndrewFilkin <filkin.andrew.s@gmail.com>
2023-04-18 20:44:13 +0300

    Add

--- app/Http/Controllers/Parsing/ParsingDataWithSiteController.php
+++ app/Http/Controllers/Parsing/ParsingDataWithSiteController.php
@@ -20,10 +20,43 @@ class ParsingDataWithSiteController extends Controller
         // Создайте объект DOM на основе HTML-кода
         $dom = HtmlDomParser::str_get_html($html);
 
+        // find all link
+        foreach ($dom->find('a') as $e)
+            echo $e->href . '<br>';
 
 // find all image
-        foreach($dom->find('img') as $e)
+        foreach ($dom->find('img') as $e)
             echo $e->src . '<br>';
+
+// find all image with full tag
+        foreach ($dom->find('img') as $e)
+            echo $e->outertext . '<br>';
+
+// find all div tags with id=gbar
+        foreach ($dom->find('div#gbar') as $e)
+            echo $e->innertext . '<br>';
+
+// find all span tags with class=gb1
+        foreach ($dom->find('span.gb1') as $e)
+            echo $e->outertext . '<br>';
+
+// find all td tags with attribite align=center
+        foreach ($dom->find('td[align=center]') as $e)
+            echo $e->innertext . '<br>';
+
+        echo '-------------------------------' . '<br>';
+        foreach ($dom->find('span') as $key => $e) {
+            if ($key == 2) {
+                echo $e->outertext . '<br>';
+            }
+        }
+
+// extract text from table
+//        echo $dom->find('td[align="center"]', 1)->plaintext . '<br><hr>';
+
+// extract text from HTML
+//        echo $dom->plaintext;
+
 
     }
 
