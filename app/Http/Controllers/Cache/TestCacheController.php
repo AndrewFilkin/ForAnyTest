f37c89ca29730855187f47694b51573daed13ffe
AndrewFilkin <filkin.andrew.s@gmail.com>
2023-05-01 16:14:57 +0300

    git test catch

--- app/Http/Controllers/Cache/TestCacheController.php
+++ app/Http/Controllers/Cache/TestCacheController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Cache;
 
 use App\Http\Controllers\Controller;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 
@@ -10,9 +11,19 @@ class TestCacheController extends Controller
 {
     public function index()
     {
-        Cache::put('my_key', 'value123', now()->addMinute(1));
-        Cache::put('my_key1', 'value123', now()->addMinute(1));
-        $value = Cache::get('my_key');
-        echo $value;
+//        $data = ['apple', 'orange', 'banana'];
+//        Cache::put('my_key', $data, 60);
+
+
+        $asd = Cache::remember('test', Carbon::now()->addMinute(1), function () {
+            return ['apple', 'orange', 'banana'];
+        });
+
+//        echo Cache::get('test');
+
+
+//        Cache::put('my_key', 'value123', now()->addMinute(1));
+//        $value = Cache::get('my_key');
+//        echo $value;
     }
 }
