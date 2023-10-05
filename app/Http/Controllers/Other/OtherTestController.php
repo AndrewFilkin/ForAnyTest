56311b56e62edbcbde83e07536b538f64d785feb
AndrewFilkin <filkin.andrew.s@gmail.com>
2023-10-05 21:26:37 +0300

    test resource

--- app/Http/Controllers/Other/OtherTestController.php
+++ app/Http/Controllers/Other/OtherTestController.php
@@ -6,14 +6,23 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Actions\TestAction;
 use App\Contracts\TestContract;
+use App\Http\Resources\UserResource;
 
 class OtherTestController extends Controller
 {
 
     public function index(TestContract $action)
     {
-        $obj = new TestAction();
-        echo $action->testContractFunction();
+//        $obj = new TestAction();
+//        echo $action->testContractFunction();
+        $mas = [
+            'title' => 'title',
+            'message' => 'mess',
+            'name' => 'name'
+        ];
+
+        return new UserResource($mas);
+
 
     }
 }
